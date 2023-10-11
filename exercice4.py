52e931229ac12424c25a7eb5a8efe6ec82cc20d4
Salah Eddine Zedadra <59291492+itzsalah24@users.noreply.github.com>
2023-10-11 18:36:11 +0200

    Add files via upload

---
+++ exercice4.py
@@ -0,0 +1,18 @@
+width = 17
+height = 12.0
+
+a_1= width // 2
+print('Value : ', a_1)  
+print('Type : ', type(a_1))
+
+a_2= width / 2
+print('Value : ', a_2)  
+print('Type : ', type(a_2))
+
+a_3= height / 3
+print('Value : ', a_3)  
+print('Type : ', type(a_3))
+
+a_4= 1 + 2 * 5
+print('Value : ', a_4)  
+print('Type : ', type(a_4))
---
+++ exercice5.py
@@ -0,0 +1,3 @@
+temp_dgr = float(input("Enter tempurature in Celsuis : "))
+temp_fhr = (temp_dgr * 9/5) + 32
+print('Temperature in Fahrenheit',temp_fhr,"ºF")
